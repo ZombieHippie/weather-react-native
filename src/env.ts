0b48fb5088fee76084a6a3d389a8439a6887011a
Cole R Lawrence <msgzht@gmail.com>
2017-03-15 16:52:09 -0500

    add(env): Create environment file

---
+++ src/env.ts
@@ -0,0 +1,9 @@
+interface EnvironmentConfig {
+  OpenWeatherMapAPIKey: String,
+  WeatherCityID: String
+}
+
+export default {
+  OpenWeatherMapAPIKey: "",
+  WeatherCityID: "4273837", // Kansas City!
+} as EnvironmentConfig
